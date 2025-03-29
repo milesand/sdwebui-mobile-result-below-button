fb790e3a4bbbc152a6eff602d799629a5cc7cd52
Jewoo Lee <me@dicemice.me>
2025-03-29 19:14:49 +0900

    initial commit

---
+++ javascript/mobile.js
@@ -0,0 +1,33 @@
+function onLoad() {
+    let values = {};
+    ['txt', 'img'].forEach((mode) => {
+        let modeValues = {};
+        modeValues.genResult = document.getElementById(mode + '2img_results');
+        modeValues.genResultParent = modeValues.genResult.parentNode;
+        modeValues.topRow = document.getElementById(mode + '2img_toprow');
+        values[mode] = modeValues;
+    })
+
+    const to_mobile = (mode) => {
+        values[mode].topRow.append(values[mode].genResult);
+    };
+
+    const undo_mobile = (mode) => {
+        values[mode].genResultParent.append(values[mode].genResult);
+    };
+
+    const media_query = matchMedia("(max-width:43.95em)");
+    if (media_query.matches) {
+        ['txt', 'img'].forEach(to_mobile);
+    }
+
+    media_query.addEventListener("change", (e) => {
+        if (e.matches) {
+            ['txt', 'img'].forEach(to_mobile);
+        } else {
+            ['txt', 'img'].forEach(undo_mobile);
+        }
+    })
+}
+
+onUiLoaded(async () => onLoad());
